cafa286801b809c64f86476a0ffc46f16c2551b5
Irina2304 <110673165+Irina2304@users.noreply.github.com>
2023-08-06 16:58:22 +0200

    task_1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
+
 console.log(galleryItems);
-console.log(galleryItems);
+
+const container = document.querySelector(".gallery");
+
+const marcup = galleryItems.map(
+    ({ preview, original, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`).join('');
+
+container.insertAdjacentHTML("beforeend", marcup);
+
+
+let lightbox = new SimpleLightbox(".gallery a", {
+captionsData: "alt",
+captionPosition: "bottom",
+captionDelay: 250,
+});
+
