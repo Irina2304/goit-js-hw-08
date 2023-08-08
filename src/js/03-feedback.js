8faa3d6192d158cee50e4bb71ca97bd2c53d570b
Irina2304 <110673165+Irina2304@users.noreply.github.com>
2023-08-08 08:31:55 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const key = 'feedback-form-state';
+
+form.addEventListener('input', throttle(inputValue, 500));
+form.addEventListener('submit', formSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(key)) || {};
+const { email, message } = form.elements;
+reload();
+
+function inputValue(evt) {
+    dataForm = { email: email.value, message: message.value };
+    localStorage.setItem(key, JSON.stringify(dataForm));
+    }
+
+function formSubmit(evt) {
+    evt.preventDefault();
+    console.log({ email: email.value, message: message.value });
+
+    if (email.value === '' || message.value === '') {
+        return alert(`All fields must be filled!`);
+    }
+
+    localStorage.removeItem(key);
+    dataForm = {};
+    evt.currentTarget.reset();
+}
+
+function reload() {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+  }
+}
